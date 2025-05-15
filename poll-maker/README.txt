e17db260944c069e3440f73295de09563cca83a4
Sean Murphy <sean@iamseanmurphy.com>
2025-05-15 09:19:07 +0000

    5.8.2

--- poll-maker/README.txt
+++ poll-maker/README.txt
@@ -4,7 +4,7 @@ Donate link: https://ays-pro.com/wordpress/poll-maker
 Tags:  WordPress Poll, vote, poll plugin, polls, polling
 Requires at least: 4.0
 Tested up to: 6.8.1
-Stable tag: 5.8.1
+Stable tag: 5.8.2
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-3.0.html
 
@@ -353,6 +353,10 @@ By this, you will save your already created data (quizzes with questions). You c
 
 
 == Changelog ==
+
+= 5.8.2 ( May 14, 2025 ) =
+* Improved: Some styles on the admin dashboard
+* Updated: POT and Po files
 
 = 5.8.1 =
 * Changed: The location of the Save as default button on the polls edit page
--- poll-maker/poll-maker-ays.php
+++ poll-maker/poll-maker-ays.php
@@ -16,7 +16,7 @@ ob_start();
  * Plugin Name:       Poll Maker
  * Plugin URI:        https://ays-pro.com/wordpress/poll-maker/
  * Description:       Poll Maker is a powerful plugin for creating custom polls and gathering opinions. It lets you design and share polls quickly to engage your audience and collect feedback.
- * Version:           5.8.1
+ * Version:           5.8.2
  * Author:            Poll Maker Team
  * Author URI:        https://ays-pro.com/
  * License:           GPL-2.0+
@@ -35,7 +35,7 @@ if (!defined('WPINC')) {
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('POLL_MAKER_AYS_VERSION', '5.8.1');
+define('POLL_MAKER_AYS_VERSION', '5.8.2');
 define('POLL_MAKER_AYS_NAME', 'poll-maker-ays');
 
 if (!defined('POLL_MAKER_AYS_DIR')) {
@@ -153,6 +153,13 @@ function poll_maker_admin_notice() {
                                 <span><?php echo esc_html__( "Upgrade", "poll-maker" ); ?></span>
                             </a>
                             <span class="ays-poll-logo-container-one-time-text"><?php echo esc_html__( "One-time payment", "poll-maker" ); ?></span>
+                        </div>
+                        <div class="ays-poll-coupon-container">
+                            <div class="ays-poll-coupon-box ays-poll-copy-element-box-parent">
+                                <!-- <img src="<?php echo esc_attr(POLL_MAKER_AYS_ADMIN_URL . '/images/icons/receipt-solid.svg'); ?>" class="ays-poll-svg-light-hover"> -->
+                                <span onClick="selectAndCopyElementContents(this)" class="ays-poll-copy-element-box" data-toggle="tooltip" title="<?php echo esc_html__( "Click for copy", 'poll-maker' ); ?>"><?php echo esc_html__( "spring2025", 'poll-maker' ); ?></span>
+                            </div>
+                            <span class="ays-poll-logo-container-one-time-text"><?php echo esc_html__( "Extra 20% Coupon", 'poll-maker' ); ?></span>
                         </div>
                     </div>
                     <ul id="menu">
